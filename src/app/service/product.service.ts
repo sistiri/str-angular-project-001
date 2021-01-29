d8f11c2d803dac14a69ac3c28949ca909f95ee29
sistiri <sistiri@yahoo.com>
2021-01-29 14:02:22 +0100

    server mock data json

---
+++ src/app/service/product.service.ts
@@ -0,0 +1,13 @@
+import { JsonPipe } from '@angular/common';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor() { }
+
+  
+  // list: Product [] = JSON.parse(require("../../server/db/products.json"))
+}

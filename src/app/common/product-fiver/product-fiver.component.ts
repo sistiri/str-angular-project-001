b3ae8ff0213f0f891a9e45c0e32276d37191b1bd
sistiri <sistiri@yahoo.com>
2021-01-30 17:58:56 +0100

    home.component es product-fiver corrections

--- src/app/common/product-fiver/product-fiver.component.ts
+++ src/app/common/product-fiver/product-fiver.component.ts
@@ -12,7 +12,7 @@ export class ProductFiverComponent implements OnInit {
 
   @Input() product: Product = new Product();
 
-  productList: Product[] = this.productService.list;
+  @Input() productList: Product[] = this.productService.list;
 
   constructor(
     private productService: ProductService,

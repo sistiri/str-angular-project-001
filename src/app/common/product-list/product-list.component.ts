a0bffd459079e274c2299209d19fb89c9e92881f
sistiri <sistiri@yahoo.com>
2021-02-01 15:01:27 +0100

    product-list component and other corrections

--- src/app/common/product-list/product-list.component.ts
+++ src/app/common/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
   @Input() product: Product = new Product();
   @Input() productList: Product[] = this.productService.list;
   @Input() phraseString: string = '';
+  @Input() categoryId: number = null;
  
 
 

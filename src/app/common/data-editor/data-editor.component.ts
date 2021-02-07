10e2b0da4915f85373f60d1fb83045740a9426d6
Zsofi <galambvarizsofia@gmail.com>
2021-02-07 12:02:56 +0100

    data-editor div created

--- src/app/common/data-editor/data-editor.component.ts
+++ src/app/common/data-editor/data-editor.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Product } from 'src/app/model/product';
+import { ConfigService, ITableCol } from 'src/app/service/config.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-data-editor',
@@ -7,7 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataEditorComponent implements OnInit {
 
-  constructor() { }
+  productList$: Observable<Product[]> = this.productService.getAll();
+  cols: ITableCol[] = this.config.tableCols;
+
+
+  constructor(
+    private config: ConfigService,
+    private productService: ProductService,
+  ) { }
 
   ngOnInit(): void {
   }

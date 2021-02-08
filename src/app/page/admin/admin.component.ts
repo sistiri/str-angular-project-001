72831d1392a3436a7ca8ca39bf6808584b4c0445
mjemand <mechle.tamas@gmail.com>
2021-02-08 02:34:51 +0100

    buttons

--- src/app/page/admin/admin.component.ts
+++ src/app/page/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/service/product.service';
 
 
 @Component({
@@ -9,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 
 export class AdminComponent implements OnInit {
 
-  constructor() { }
-  
+  constructor(
+    private productService: ProductService,
+  ) {}
+
   ngOnInit(): void {
   }
-  
+
+  onUpdate(product: Product): void {
+    this.productService.update(product);
+  }
+
+  onDelete(product: Product): void {
+    this.productService.remove(product);
+  }
+
+  onPlus(product: Product): void {
+    this.productService.add(product);
+  }
+
 }
